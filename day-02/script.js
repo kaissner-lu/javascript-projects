7467c33cf02b097aad4fd89d2abd673842de919d
kaissner-lu <l.kaissner@gmail.com>
2023-06-21 12:25:54 +0200

    fix: adds proper prefix to html file, error fixes

---
+++ day-02/script.js
@@ -0,0 +1,19 @@
+console.log("L" + "u" + "i" + "s" + "a");
+
+//Task to spot errors
+
+console.log("Hallo");
+
+console.log("That doesn´t work");
+
+console.log(50 + 2 === 52);
+
+console.log("333" === 333);
+
+console.log((600 + 510) / 2);
+
+console.log(2 + 2);
+
+//Compare conditions
+
+console.log(25 / 5 === 50 / 10);
